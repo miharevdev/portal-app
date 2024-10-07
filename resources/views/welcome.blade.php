3788277e74866d8ea08a6c20f8f8cdab4efeb271
m.pisarev <m.pisarev@smticc.ru>
2024-10-07 17:47:26 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                HELLO PAGE!
+                HELLO PAGE !!!
             </div>
         </div>
     </body>
